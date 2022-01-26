34c6d78363eda83d4d457da45d240fe2ec1c96e3
Waldemar Penner <waldemar.penner91@gmail.com>
2022-01-26 22:21:06 +0100

    add jokes route

---
+++ src/routes/jokes.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ fetch }) => {
+		const response = await fetch('https://v2.jokeapi.dev/joke/Any?type=twopart');
+		const data = await response.json();
+		return {
+			props: {
+				setup: data.setup,
+				delivery: data.delivery
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let setup;
+</script>
+
+<h1>{setup}</h1>
